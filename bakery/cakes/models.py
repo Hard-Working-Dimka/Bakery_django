95bb491750dbbd7fdbff7f7a2d1983256d8a8b1e
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-02-27 01:12:09 +0300

    upgrade models

--- bakery/cakes/models.py
+++ bakery/cakes/models.py
@@ -7,43 +7,48 @@ class CustomUser(AbstractUser):
     telegram_username = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return self.username
-
-
-class Cake(models.Model):
-    name = models.CharField(max_length=100)
-    ingredients = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=10)
-
-    def __str__(self):
-        return self.name
+        return f"{self.username} - {self.telegram_username}"
 
 
 class Modifications(models.Model):
-    modification = models.TextField()
+    modification = models.CharField(max_length=50)
+    necessary = models.BooleanField()
 
     def __str__(self):
         return self.modification
 
 
 class VariablesOfModification(models.Model):
-    modification = models.ForeignKey(Modifications, on_delete=models.CASCADE)
+    modification = models.ForeignKey(Modifications, on_delete=models.CASCADE, related_name='all')
     tier = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
-        return f'{self.modification} --- {self.tier}'
+        return f'{self.modification} - {self.tier}'
+
+
+class Cake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    ingredients = models.TextField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    photo = models.ImageField(null=True, blank=True, upload_to='cake_photos')
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
-    address = models.TextField()
+    address = models.CharField(max_length=50)
     status = models.BooleanField(default=False)
-    cake = models.ForeignKey(Cake, on_delete=models.CASCADE, blank=True, null=True)
-    cake_builder = models.ManyToManyField(VariablesOfModification, blank=True, null=True)
-    delivery_date = models.DateField()
-    phone_number = models.TextField()
+    cake = models.ForeignKey(Cake, on_delete=models.CASCADE, blank=True, null=True, related_name='orders')
+    variables_of_modifications = models.ManyToManyField(VariablesOfModification, blank=True, null=True,
+                                                        related_name='orders')
+    delivery = models.DateTimeField()
+    phone_number = models.BigIntegerField()
     created = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.customer} --- {self.cake}"
+        return f"{self.customer}"
