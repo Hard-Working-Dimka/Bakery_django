4736a62486e5f7e34fb6dee117e76e50c56649eb
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-02-28 10:52:56 +0300

    upgrade API

--- bakery/API/serializers.py
+++ bakery/API/serializers.py
@@ -27,6 +27,8 @@ class CakeSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     customer = serializers.SlugRelatedField(slug_field='telegram_id', queryset=CustomUser.objects.all())
     cake = serializers.SlugRelatedField(slug_field='name', queryset=Cake.objects.all())
+    variables_of_modifications = serializers.SlugRelatedField(slug_field='tier',
+                                                              queryset=VariablesOfModification.objects.all(), many=True)
 
     class Meta:
         model = Order
